389924f8ed8b6107e5ae2fd5faad125193ec2436
Iago Anjos <iagokaued.com>
2025-10-06 20:09:42 -0300

    feat: create Container layout component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from './Container';
+import './styles/global.css';
+import './styles/theme.css';
+
+export function App() {
+  return (
+    <>
+      <Container>
+        <section>test</section>
+      </Container>
+    </>
+  );
+}
