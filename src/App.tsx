67e868af6a2e22e104a2db5903cbb45fbce7d33e
Iago Anjos <iagokaued.com>
2025-10-10 12:12:59 -0300

    feat: create NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { AboutPomodoro } from './pages/AboutPomodoro';
+import { NotFound } from './pages/NotFound';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <AboutPomodoro />
+      <NotFound />
     </>
   );
 }
